6e0f5ea84baacced3f823d21ddd18d15e4b4260e
Antti Virtanen <antti.virtanen@iki.fi>
2015-04-21 14:43:08 +0300

    OPH-1336 sovelluspalvelimen alustusskripti CI-buildia varten

---
+++ env/app-server.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -eu
+
+if [ $# -ne 2 ]
+then
+    echo "$0 <ympäristön-asetusten-polku> <deploy_id_rsa.pub>"
+    exit 1
+fi
+
+system='osaan'
+
+env_dir=$1
+id_rsa_pub=$2
+admin_user="${system}admin"
+
+set -x
+
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+
+software/jre.sh
+
+useradd $admin_user
+
+# admin-käyttäjälle oikeudet ajaa rootina asennukseen tarvittavat komennot
+# ilman salasanaa
+echo "$admin_user ALL = NOPASSWD: ALL" >> /etc/sudoers
+
+# Sallitaan asennusten pääsy ssh:lla
+mkdir /home/$admin_user/.ssh
+cat $id_rsa_pub >> /home/$admin_user/.ssh/authorized_keys
+
+chown -R $admin_user:$admin_user /home/$admin_user/.ssh
+chmod 700 /home/$admin_user/.ssh
+chmod 644 /home/$admin_user/.ssh/authorized_keys
+
+$env_dir/app-server/setup.sh
---
+++ env/software/jre.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+yum install -y java-1.8.0-openjdk
+yum install -y java-1.8.0-openjdk-devel # jar-komento
