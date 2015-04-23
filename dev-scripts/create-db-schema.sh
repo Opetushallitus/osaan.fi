3ee0a0a5f835693418db61a7bbeaaa7e55af3bf8
Mikko Suonio <mikko.suonio@solita.fi>
2015-04-23 10:56:52 +0300

    OPH-1337: Lataa testidata skeeman luonnissa

--- dev-scripts/create-db-schema.sh
+++ dev-scripts/create-db-schema.sh
@@ -6,7 +6,7 @@ repo_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 set -x
 
 cd $repo_path/db
-lein run 'postgresql://osaan_adm:osaan-adm@127.0.0.1:4567/osaan' -u osaan_user --clear $@
+lein run 'postgresql://osaan_adm:osaan-adm@127.0.0.1:4567/osaan' -u osaan_user --clear -t $@
 
 cd $repo_path/ansible
 chmod 600 yhteiset/dev_id_rsa
