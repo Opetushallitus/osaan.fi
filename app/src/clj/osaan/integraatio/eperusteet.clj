899a635afa60827539869d8788962e4bbc5358ae
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2015-05-07 10:53:04 +0300

    OPH-1342 Kaikkien perusteiden haku

--- app/src/clj/osaan/integraatio/eperusteet.clj
+++ app/src/clj/osaan/integraatio/eperusteet.clj
@@ -13,10 +13,19 @@
 ;; European Union Public Licence for more details.
 
 (ns osaan.integraatio.eperusteet
- (:require [clj-time.core :as time]
+ (:require [clojure.tools.logging :as log]
+           [clj-time.core :as time]
            [clj-time.coerce :as c]
-           [oph.common.util.util :refer :all]
-           [clojure.tools.logging :as log]))
+           [oph.common.util.util :refer :all]))
+
+(defn lataa-kaikki-sivut [url options]
+  (loop [vanha-data []
+         sivu 0]
+    (let [{:keys [data sivuja]} (get-json-from-url url (assoc-in options [:query-params :sivu] sivu))
+          data (concat vanha-data data)]
+      (if (= (inc sivu) sivuja)
+        data
+        (recur data (inc sivu))))))
 
 (defn muotoile-arvioinnin-kohde [index alue]
   {:jarjestys (inc index)
@@ -41,7 +50,18 @@
    :voimassa_alkupvm (c/from-long (:voimassaoloAlkaa peruste))
    :voimassa_loppupvm (c/from-long (:voimassaoloLoppuu peruste))
    :siirtymaajan_loppupvm (c/from-long (:siirtymaPaattyy peruste))
-   :tutkinnonosat (map-indexed muotoile-tutkinnonosa (:tutkinnonOsat peruste))})
+   :tutkinnonosat (map-indexed muotoile-tutkinnonosa (:tutkinnonOsat peruste))
+   :suoritustavat (map :suoritustapakoodi (:suoritustavat peruste))})
 
 (defn hae-peruste [id asetukset]
   (muotoile-peruste (get-json-from-url (str (:url asetukset) "api/perusteet/" id "/kaikki"))))
+
+(defn hae-perusteet [viimeisin-haku asetukset]
+  (for [peruste (lataa-kaikki-sivut (str (:url asetukset) "api/perusteet") {:query-params {:muokattu (c/to-long viimeisin-haku)}})
+        :let [peruste-data (hae-peruste (:id peruste) asetukset)]
+        tutkinto (:koulutukset peruste)
+        suoritustapa (:suoritustavat peruste-data)]
+    (-> peruste-data
+      (assoc :tutkinto (:koulutuskoodiArvo tutkinto)
+             :tyyppi suoritustapa)
+      (dissoc :suoritustavat))))
