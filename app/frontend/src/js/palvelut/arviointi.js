58cd1f430a0831e06220e9fca7c66e2a2407108c
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2015-05-07 15:11:07 +0300

    Turhat hipsut pois objektin avaimen ympäriltä.

--- app/frontend/src/js/palvelut/arviointi.js
+++ app/frontend/src/js/palvelut/arviointi.js
@@ -19,7 +19,7 @@ angular.module('osaan.palvelut.arviointi', [])
   .factory('Arviointi', [function() {
     var tila = {
       arviot: {}, // tutkinnonosatunnus -> arvioinninkohde_id -> {arvio, vapaateksti}
-      'osatunnukset': []
+      osatunnukset: []
     };
     var _tallennaTila = function() {
       sessionStorage.setItem('arviointi', JSON.stringify(tila));
