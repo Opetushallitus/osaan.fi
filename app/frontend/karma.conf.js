ca2585ddf5abb95b46cd7ff0f915147e0ae5d8ce
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2015-05-29 08:59:50 +0300

    Poista turha angular-cookies -riippuvuus

--- app/frontend/karma.conf.js
+++ app/frontend/karma.conf.js
@@ -35,7 +35,6 @@ module.exports = function(config) {
       'src/bower_components/angular/angular.js',
       'src/bower_components/select2/select2.js',
       'src/bower_components/angular-animate/angular-animate.js',
-      'src/bower_components/angular-cookies/angular-cookies.js',
       'src/bower_components/angular-route/angular-route.js',
       'src/bower_components/angular-resource/angular-resource.js',
       'src/bower_components/angular-loading-bar/src/loading-bar.js',
