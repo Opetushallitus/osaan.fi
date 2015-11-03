2ac5fcc97d67304b23d3d9586f3b19d25a22974a
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2015-11-03 12:24:20 +0200

    OPH-1569: Uusi ohjeteksti etusivulle

---
+++ db/resources/db/migration/V1_5__ohjeteksti_etusivu.sql
@@ -0,0 +1,4 @@
+update ohje set
+    teksti_fi = 'Valitse tutkinto, jonka tutkinnon perusteisiin vertaat osaamistasi. Jos et löydä tutkintoa tästä palvelusta, löydät sen vanhasta Osaan.fi:stä, jonne pääset näytöllä näkyvästä linkistä.',
+    teksti_sv = 'Välj den examen mot vars examensgrunder du vill jämföra ditt kunnande. Om du inte hittar examen i denna tjänst, hittar du den i den gamla Jagkan.fi-tjänsten som du kommer åt genom att klicka på länken som syns på skärmen.'
+    where ohjetunniste = 'etusivu';
