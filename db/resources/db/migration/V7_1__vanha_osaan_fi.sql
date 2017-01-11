3860c5433ec0217b2b10032850704731e667995f
Antti Virtanen <antti.virtanen@solita.fi>
2017-01-11 10:46:00 +0200

    OPH-1918 poistettu maininnat vanhasta osaan.fi palvelusta ja poistettu linkki siihen.

---
+++ db/resources/db/migration/V7_1__vanha_osaan_fi.sql
@@ -0,0 +1,4 @@
+update ohje set
+    teksti_fi = 'Valitse tutkinto, jonka tutkinnon perusteisiin vertaat osaamistasi.',
+    teksti_sv = 'Välj den examen mot vars examensgrunder du vill jämföra ditt kunnande.'
+    where ohjetunniste = 'etusivu';
