0b4b15a867d83736e547181420f4e83047213466
Niko Junkala <niko.junkala@gofore.com>
2018-05-23 12:18:34 +0300

    Korjaus migraatioon

--- db/resources/db/migration/V8_1__isced-luokitus.sql
+++ db/resources/db/migration/V8_1__isced-luokitus.sql
@@ -6,9 +6,8 @@ update koulutusala set voimassa_loppupvm = to_date('01-07-2017', 'DD-MM-YYYY');
 -- Testitapausten kannalta tietokanta on tyhjä ja insert ei tapahdu. Asennuksessa olemassaolevan tietokannan päälle tapahtuu.
 insert into opintoala(opintoalatunnus, koulutusala, nimi_fi, voimassa_alkupvm, voimassa_loppupvm)
   select '007', koulutusalatunnus, 'Keksitty opintoala ISCED-päivitystä varten', to_date('2017-01-01','YYYY-MM-DD'), to_date('2017-01-01','YYYY-MM-DD')
-  from koulutusala where koulutusalatunnus = '0';
+  from koulutusala where koulutusalatunnus = '0' on conflict do nothing;
 
-  
 update tutkinto set opintoala = '007' where tutkintotunnus not in
 ('321603','377111','371113','354116','351108','354204','010001','324503');
 
